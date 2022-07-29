464201b27ebf7533437c959e7650912f6a54c5a8
Evan Yohe <36523433+EvanYohe@users.noreply.github.com>
2022-07-29 15:03:13 -0700

    Updated commenting in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-// Search a collapsed container of elements for an element matching input
+// Initialize all the collapsibile elements on the page
+// Add an event listener 'input' to the searchbar
+// On event, compare input text to list of collapsables and expand if input text matches collapsable element id
+
 const collapseElementList = document.querySelectorAll('.collapse')
 const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
 
